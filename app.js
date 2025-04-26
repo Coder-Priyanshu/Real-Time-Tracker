b6828d0d4cf677f4c6dd71c4a7e7f4b41fde4638
Coder-Priyanshu <priyanshu.empire1234@gmail.com>
2025-04-26 13:05:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express =require('express');
+const app = express();
+const http = require("http");
+const path = require('path');
+
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine","ejs" );
+app.set(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket){
+    socket.on("send-location", function(data){
+        io.emit("receive-loaction",{id: socket.id, ...data});
+    });
+    console.log("Connected");
+    socket.on("disconnected", function(){
+        io.emit("user-disconnected")
+    })
+});
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+server.listen(3000); 
